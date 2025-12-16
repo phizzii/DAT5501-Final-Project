307aedd48b3af2c08daa3d0604d54f059236cd18
Sophie <109428682+phizzii@users.noreply.github.com>
2025-12-16 11:49:47 +0000

    creating scripts to sample datasets which are unfortunately very very large for a baseline model and analysis, first 2 scripts for beauty and fashion products on amazon (reviews)

--- scripts/amazon_reviews.py
+++
@@ -1,4 +0,0 @@
-from datasets import load_dataset
-
-dataset_stream = load_dataset("parquet", data_files="https://huggingface.co/datasets/McAuley-Lab/Amazon-Reviews-2023/resolve/main/raw_review_All_Beauty/*.parquet", split="train", streaming=True)
-
---
+++ scripts/create_amazon_beauty_reviews_datasets.py
@@ -0,0 +1,18 @@
+import random
+from itertools import islice
+from datasets import load_dataset
+from datasets import Dataset
+
+dataset_stream = load_dataset("json", data_files="hf://datasets/McAuley-Lab/Amazon-Reviews-2023/raw/review_categories/All_Beauty.jsonl", split="train", streaming=True)
+
+first = next(iter(dataset_stream))
+print(first.keys())
+
+random.seed(42)
+
+sample_size = 10_000
+
+sampled = list(islice(dataset_stream.shuffle(buffer_size=100_000, seed=42), sample_size))
+
+dataset = Dataset.from_list(sampled)
+print(dataset)
