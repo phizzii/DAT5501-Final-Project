2a00dd927fd89f6adaf43b97ba7ad950246735ce
Sophie <109428682+phizzii@users.noreply.github.com>
2025-12-16 13:09:45 +0000

    FINALLy fixed the issues with columns and their types (it was a the user column having numbers)

--- scripts/create_steam_reviews_dataset.py
+++ scripts/create_steam_reviews_dataset.py
@@ -1,8 +1,17 @@
 import os
+from h11 import Data
 import pandas as pd
 import random
 from datasets import Dataset
-from langdetect import detect
+from langdetect import detect, DetectorFactory, LangDetectException
+
+DetectorFactory.seed = 42
+
+def safe_detect(text):
+    try:
+        return detect(text)
+    except LangDetectException:
+        return "unknown" # marking up reviews which can't be detected
 
 # path to the unzipped steam review files in raw data folder (which is in gitignore)
 steam_reviews_dir = "raw_data/Game Reviews"
@@ -29,9 +38,17 @@ for file in files:
 
     dataframe = dataframe.dropna(subset=["review"])
 
-    dataframe["lang"] = dataframe["review"].apply(lambda x: detect(x))
+    dataframe["review"] = dataframe["review"].astype(str)
+    
+    dataframe = dataframe[dataframe["review"].str.strip() != ""] # removing whitespace and empty things since it complains if i don't
+
+    dataframe["lang"] = dataframe["review"].apply(safe_detect)
 
     dataframe = dataframe[dataframe["lang"] == "en"]
+
+    for col in ["user", "post_date", "review", "recommend", "early_access_review"]:
+        if col in dataframe.columns:
+            dataframe[col] = dataframe[col].fillna("").astype(str)
 
     dataframe = dataframe.sample(frac=1, random_state=42)
 
