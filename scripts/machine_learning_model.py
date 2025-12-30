aad3b83ad3d93b5f7d2e45336161fbb6cbaa9175
Sophie <109428682+phizzii@users.noreply.github.com>
2025-12-30 14:36:50 +0000

    more comments about le plan including features and models i want to use and evaluation metrics

--- scripts/machine_learning_model.py
+++ scripts/machine_learning_model.py
@@ -8,4 +8,9 @@
 
 # putting raw text isn't really plausible so there's going to be sentiment features which are interpretability focused like the proportion of negative words used (business meaning being how emotionally negative is the review?)
 # linguistic intensity features, like review length (word count), maybe number of symbols used (like exclamation marks), how much capitalisation is there?, use of intensifiers like 'very', 'extremely' or 'worst' (business meaning being how strongly is the user expressing negativity?)
-# structural features, like number of stars or how many people found the review useful?
+# structural features, like number of stars or how many people found the review useful? (not sure about that one though), platform indicator (business meaning being where is the dissatisfaction coming from)
+# optional lexical features, like frequency of complaint related terms like refunds, broken or crash (but only to help not dominate)
+
+# model options (all used not just one): decision tree (because feature importance for business insight), random forest (for robustness and performance comparison), logistic regression (as a baseline checker and interpretability)
+
+# evaluation metrics: the aim is not for perfect accuracy so i'm going to use precision because sometimes models can give false positives!! (which would be costly for a business), recall because sometimes negative reviews may be missed which happens to also be costly to the business (you can see where this is going)
