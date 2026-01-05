7bab8579e3d2c60d3d436614f12bb8da7ced9658
Sophie <109428682+phizzii@users.noreply.github.com>
2026-01-05 19:15:47 +0000

    proper commenting

--- scripts/create_yelp_reviews_dataset.py
+++ scripts/create_yelp_reviews_dataset.py
@@ -1,14 +1,21 @@
+# import necessary modules
 from datasets import load_dataset
 import random
 
+# gets the yelp full review dataset, loads only training split
 dataset = load_dataset("Yelp/yelp_review_full", split="train")
 
+# prints features so i know what columns there are (not many...)
 print(dataset.features)
 
-random.seed(33) # fav number, not technically important
+# set random seed for reproducibility, 33 is my favourite number, not technically important
+random.seed(33)
 
+# set sample size, again bigger than the amazon ones since there are many of those and only one of this one
 sample_size = 25_000
-dataset_sampled = dataset.shuffle(seed=42).select(range(sample_size))
+
+# randomly reorders dataset and takes first 25k rows after shuffling
+dataset_sampled = dataset.shuffle(seed=33).select(range(sample_size))
 
 print(dataset_sampled)
 
